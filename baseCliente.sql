6949036e75c6aa9d3bc6f0dc4b1d872bf59da57c
WestBern <42044496+WestBern@users.noreply.github.com>
2019-05-13 17:43:34 -0300

    Delete baseCliente.sql

--- baseCliente.sql
+++
@@ -1,7 +0,0 @@
-use tcc;
-select * from cliente;,
-insert into cliente values(8,'Raphael','2001-11-05','Augusto Franco','sen22ha1','use12r1');
-insert into cliente values(null,'Lucas','2005-11-05','Augusto ','sen22ha1','us123er1');
-insert into cliente values(null,'tantas','2004-11-05',' Franco','sen22ha1','use123r1');
-insert into cliente values(null,'sdasdasd','2045-11-05','uu Franco','senh22a1','us123er1');
-insert into cliente values(null,'asd','2006-11-05',' Franco','senha21','user1231');
