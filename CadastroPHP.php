bd67d1387782c8b881927dd7267a45e801e988b6
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-05-23 13:40:12 -0300

    LOGIN $_SESSION INDEX|LOGIN

---
+++ CadastroPHP.php
@@ -0,0 +1,47 @@
+
+<?php
+session_start();
+
+include_once('config.php');
+
+$user = $_GET['username'];
+$pass = $_GET['password'];
+$end = $_GET['endereço'];
+$nca = $_GET['ncasa'];
+$data = $_GET['data'];
+$nome = $_GET['nome'];
+$cpf = $_GET['cpf'];
+$id = null;/*
+echo "$user <br>";
+echo "$pass <br>";
+echo "$end <br>";
+echo "$nca<br>" ;
+echo "$data<br>";
+echo "$nome<br>";
+
+echo "$id<br>";
+*/
+
+
+
+/*mysqli_query($mysqli,"INSERT INTO Cliente VALUES('669', 'asfas', '2010-02-26', 'axaf', 'fasf', 'fasfsa', '6', '42');");*/
+
+//mysqli_query($con,"INSERT INTO `Cliente`(ClienteID, Nome, Nascimento, Endere�o, Senha, Usuario, Ncasa, cpf)
+                            //              VALUES(null,'$nome','$data','$end','$pass','$user','$nca','$cpf')");
+mysqli_query($con,"INSERT INTO `Cliente`(ClienteID,Nome,Nascimento,Endereco,Senha,Usuario,Ncasa,cpf)
+VALUES(null,'$nome','$data','$end','$pass','$user','$nca','$cpf')");
+$_SESSION['logado'] = true;
+$_SESSION['nome'] = $username;
+
+//mysqli_query($con,"INSERT INTO Cliente VALUES('9','$nome')");
+
+/*mysqli_query($mysqli,"INSERT INTO Cliente VALUES(99,"$_GET['nome']",$_GET['data'],'$_GET['endere�o']','$_GET['password']','$_GET['username']','$_GET['ncasa']','$_GET['cpf']')");
+*/
+
+
+/*if (mysql_query($mysqli,"INSERT INTO cliente VALUES(3,'Rap','2001-05-11','ende','56as','user','ns','sad')" )) {
+  echo "<h1>Cadastro Completo</h1>";
+
+}*/
+
+?>
---
+++ Login.php
@@ -0,0 +1,132 @@
+
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv=”content-type” content="text/html;" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Login</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="icon"  href="https://travelpedia.com.br/wp-content/uploads/2017/11/food-icon2.png">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+  <link rel="stylesheet" type="text/css" href="estiloLogin.css">
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+
+</head>
+
+		<body>
+
+			<div class="corpo">
+
+				<nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #B2002A; border-bottom: 0px;" >
+					<div class="container">
+
+
+						<div class="navbar-header">
+
+
+						<a href="index.php">	 <img id="img_pizza_logo" src="https://cdn4.iconfinder.com/data/icons/food-and-drink-1-2/64/pizza-food-italian-meal-512.png"></a>
+							 <a href="index.php">	<p id="nome_logo">PayPizza</p></a>
+
+
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#links-menu" style="background-color: white;">
+								<i class="material-icons">menu</i>
+
+							</button>
+						</div>
+
+
+						<nav id="links-menu" class="collapse navbar-collapse">
+							<ul class="nav navbar-nav navbar-right">
+
+
+										<li><a href="cardapio.html"> <p class="menu_cabeça"> Cardápio </p></a></li>
+								<li><a href="#"> <p  class="menu_cabeça">Promoções</p></a></li>
+								<li><a href="#"> <p class="menu_cabeça">Quem Somos</p></a></li>
+
+
+								<li><a href="#" class="menu_cabeça"><i class="material-icons" style="color: white;">shopping_cart</i></a></li>
+                  <li><a href="#"> <p class="menu_cabeça"><i class="material-icons" style="color: white;">person</i></a></li>
+
+							</ul>
+						</nav>
+					</div>
+				</nav>
+
+          <section>
+
+
+                          <?php
+                          session_start();
+                        $databaseHost = 'localhost';
+                        $databaseName = 'tcc';
+                        $databaseUsername = 'PayPizza';
+                        $databasePassword = 'paypizza';
+                        $mysqli = mysqli_connect($databaseHost,
+                        $databaseUsername,$databasePassword,$databaseName);
+
+
+
+
+                        ?>
+
+
+                           <?php
+
+                                            include_once('config.php');
+                                            ?>
+
+                          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+
+                          <form method="post" action="Login.php" class="login">
+                                            <h2>Login</h2>
+                                            <div class="input-icon">
+
+                                                   <i class="material-icons "  id="chato">person</i>
+                                                      <input type="text" name="username" id="i1" class="input" placeholder="Nome de usuário"/><br><br>
+
+                                                       <i class="material-icons" id="chato">vpn_key</i>
+                                                <input type="password" required name="password" id="i2" class="input" placeholder="Senha"/><Br><br>
+                                                                    <input type="submit" id="i3" placeholder="Enviar" class="button-login"/>
+                                                                                    <p id="cadastrar">cadastrar</p>
+
+                                            </div>
+
+
+
+
+                                            </form>
+                                            <br>
+
+                                            <<?php
+                                             include_once('LoginPHP.php');
+                                                ?>
+                      </section>
+
+
+
+
+
+      </div>
+
+</body>
+</html>
