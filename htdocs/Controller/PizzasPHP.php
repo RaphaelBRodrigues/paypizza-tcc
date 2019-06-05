f1de284ef4ad81912c156df8d50b2e0ea05d6940
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-06-05 11:05:19 -0300

    Itens do cardápio sem Design funcionando

--- htdocs/Controller/PizzasPHP.php
+++ htdocs/Controller/PizzasPHP.php
@@ -1,2 +1,15 @@
 <?php
+
+
+$cons2 = mysqli_query($mysqli,"SELECT * from produto where CategoriaID = $tipo");
+          echo "<br>";
+
+          while ($imp2 = mysqli_fetch_array($cons2)) {
+
+          echo "<tr>";
+          echo "<td>" . $imp2['Nome'] . " </td>";
+          echo "<td>" . $imp2['Preco'] . " </td>";
+          echo "</tr> <br>";
+
+        }
 ?>
