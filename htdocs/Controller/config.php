0979284821fc299bb3ec4d071b5f36e27ff88ded
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-06-08 21:44:38 -0300

    Add files via upload

--- htdocs/Controller/config.php
+++ htdocs/Controller/config.php
@@ -1,4 +1,5 @@
 <?php
+//LOCAL
 $databaseHost = 'localhost';
 $databaseName = 'tcc';
 $databaseUsername = 'PayPizza';
@@ -6,6 +7,16 @@ $databasePassword = 'paypizza';
 $mysqli = mysqli_connect($databaseHost,
 $databaseUsername,$databasePassword,$databaseName);
 
+
+//EPIZY
+/*$databaseHost = 'sql203.epizy.com';
+$databaseName = 'epiz_23520784_PayPizza';
+$databaseUsername = 'epiz_23520784';
+$databasePassword = 'paypizza2019';
+$mysqli = mysqli_connect($databaseHost,
+$databaseUsername,$databasePassword,$databaseName);
+
+*/
 $con = $mysqli;
 
 
