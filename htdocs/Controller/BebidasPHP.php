12c10bb881f492181fe9c3b658667cc718200957
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-06-10 17:49:57 -0300

    Update BebidasPHP.php

--- htdocs/Controller/BebidasPHP.php
+++ htdocs/Controller/BebidasPHP.php
@@ -1,5 +1,6 @@
 <?php
 include_once('config.php');
+session_start();
 
 $cons2 = mysqli_query($mysqli,"SELECT * from Produto where CategoriaID = $tipo");
           echo "<br>";
@@ -12,12 +13,33 @@ $cons2 = mysqli_query($mysqli,"SELECT * from Produto where CategoriaID = $tipo")
           echo "<a href='Atualizar.php?id=".$imp2['ProdutoID']."'>Editar Produto</a><br>
           <form class='' action='../Controller/DeletarPHP.php' method='get'>
             <button type='submit' value=".$imp2['ProdutoID']." name='apagar'>Deletar</button>
+          </form>";echo"
+          <form class='' action='#' method='get'>
+            <button type='submit' value=".$imp2['ProdutoID']." name='comprar'>Adicionar ao carrinho</button>
           </form>";
           echo "</tr> <br><br><br>";
 
 
 
         }
+        $click = $_GET['comprar'];
+        $ClienteID = $_SESSION['ClienteID'];
+if(isset($click)){
+  mysqli_query($mysqli,"INSERT INTO compra VALUES(null,$click,$ClienteID)");
+}
+      $pedido = array();
+
+      array_push($pedido, $click);
+      $_SESSION['compra'] = $pedido;
+//print_r($array);
+       //echo $array[0];
+
+//$_SESSION[$array[0]] = $pedido ;
+$i = 0;
+while($i<10){
+  $_SESSION[$pedido[0]];
+$i = $i+1;
+}
 /*
 $cons2 = mysqli_query($mysqli,"SELECT * from produto where CategoriaID = $tipo");
           echo "<br>";
