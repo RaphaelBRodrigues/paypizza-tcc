4deb7fbe3448dfe278f19e89caa8cd676e8b6437
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-09-11 15:52:58 -0300

    Statuss FIX
    Pedidos pizzaria SIDE

--- htdocs/Controller/PedidosPizzariaPHP.php
+++ htdocs/Controller/PedidosPizzariaPHP.php
@@ -1,6 +1,6 @@
 <?php
 include("config.php");
-$dados = mysqli_query($con,"SELECT * FROM Compra INNER JOIN carrinho where Compra.Sessao = carrinho.Sessao");
+$dados = mysqli_query($con,"SELECT * FROM Compra INNER JOIN carrinho where Compra.Sessao = carrinho.Sessao and Compra.Statuss = 1");
 while($row = mysqli_fetch_array($dados)){
   echo "<br>Compra ID:".$row['CompraID'];
   echo "<br>Horário :".$row['horario'];
@@ -8,23 +8,27 @@ while($row = mysqli_fetch_array($dados)){
   $prod = mysqli_query($con,"SELECT * from carrinho inner join Produto  where Sessao = ".$row['Sessao']." and Carrinho.ProdutoID = Produto.ProdutoID ");
   echo "<BR>PRODUTOS:";
 
+
+echo "<ul>";
+  while($row1 = mysqli_fetch_array($prod)){
+    echo "<br><li>".$row1['Nome']."</li><br>";
+  }
+  echo "</ul>";
+
   echo "<br>
   <form action='../Controller/attStatusPHP.php' method='get'>
   <select>
-    <option value='0' name='status'>Cancelado</option>
-    <option value='1' name='status'>Em processo</option>
+  <option value='1' name='status'>Em processo</option>
     <option value='2' name='status'>Pronto</option>
     <option value='3' name='status'>À Caminho</option>
     <option value='4' name='status'>Entregue</option>
+    <option value='0' name='status'>Cancelado</option>
+
   </select>
   <button type='submit'>Atualizar</button>
 </form>
   ";
 
-  while($row1 = mysqli_fetch_array($prod)){
-    echo "<br>".$row1['Nome']."<br>";
-  }
-
-  echo"<br><br>";
+  echo"<hr><br><br>";
 }
  ?>
