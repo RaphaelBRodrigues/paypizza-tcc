5660f8e27a486d67cc4753b298122906386c2ca8
Raphael Barbosa Rodrigues <42044496+RaphaelBRodrigues@users.noreply.github.com>
2019-09-23 14:48:33 -0300

    Update Login.php

--- htdocs/Views/Login.php
+++ htdocs/Views/Login.php
@@ -173,7 +173,7 @@
                       				</div>
                       				<div class="mt-4">
                       					<div class="d-flex justify-content-center links">
-                      						Don't have an account? <a href="#" class="ml-2">Sign Up</a>
+                      						Don't have an account? <a href="Cadastro.php" class="ml-2">Sign Up</a>
                       					</div>
                       					<div class="d-flex justify-content-center links">
                       						<a href="#">Forgot your password?</a>
